3a2cdf1a1eb44cb496a2692887d77f7679bfd3a9
Kevin <kevinahuber@gmail.com>
2015-12-10 22:50:26 -0600

    Open to port 80

--- app.js
+++ app.js
@@ -40,9 +40,4 @@ app.get('/off', function (req, res) {
   })
 })
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+app.listen(80);
