fa9b29048d5d349cb54870cc4feba140d8e98637
Kevin <kevinahuber@gmail.com>
2015-12-11 01:01:49 -0600

    Styles and added phyweb logo

--- app.js
+++ app.js
@@ -3,27 +3,31 @@ var https = require('https');
 var request = require('request');
 require('dotenv').load({silent: true});
 var favicon = require('serve-favicon')
-
-
+var express = require('express');
 
 var app = express();
 app.set('view engine', 'jade');
-app.use(favicon(__dirname + '/views/images/favicon.ico'))
+app.use(favicon(__dirname + '/public/images/favicon.ico'))
+app.use(express.static('public'))
 
 app.get('/', function (req, res) {
+  res.render('landing', {title: 'Turn On Siren', content: 'Activate the Physical Web Siren... you know you want to.'})
+});
+
+app.get('/on', function (req, res) {
   var url = 'https://maker.ifttt.com/trigger/on_light/with/key/' + process.env.IF_KEY
   console.log(url)
   https.get(url, function (response) {
-  	response.on('data', function (chunk) {
+    response.on('data', function (chunk) {
       console.log(chunk);
-      res.render('on', {title: 'Turn On Siren'})
-  	});
-  	response.on('end',function(){
+      res.render('on', {title: 'Physical Web Siren', content: 'Woo Woo. The Physical Web Siren!'})
+    });
+    response.on('end',function(){
         console.log("\nResponse ended\n");
-  	});
-  	response.on('error', function(err){
+    });
+    response.on('error', function(err){
         console.log("Error Occurred: "+err.message);
-  	});
+    });
   })
 });
 
@@ -34,7 +38,7 @@ app.get('/off', function (req, res) {
 
   	response.on('data', function (chunk) {
       console.log(chunk);
-      res.render('off', {title: 'Physical Web Siren'});
+      res.render('off', {title: 'Physical Web Siren', content: 'Activate the Physical Web Siren... you know you want to.'});
     })
   	response.on('end',function(){
         console.log("\nResponse ended\n");
